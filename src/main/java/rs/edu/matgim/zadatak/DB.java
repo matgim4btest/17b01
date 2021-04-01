31231201f99fc0b4e7be5bd11a9f15a2da207b2d
User <User@DESKTOP-EVOU4MS>
2021-04-01 09:59:53 +0200

    ab

--- src/main/java/rs/edu/matgim/zadatak/DB.java
+++ src/main/java/rs/edu/matgim/zadatak/DB.java
@@ -23,5 +23,45 @@ public class DB {
             System.out.println(ex);
         }
     }
+    public void printPositiveRacun() throws SQLException
+    {
+        try ( Connection conn = DriverManager.getConnection(connectionString);  Statement s = conn.createStatement()) {
+            ResultSet rs = s.executeQuery("SELECT * FROM Racun WHERE Stanje > 0");
+            while (rs.next()) {
+                int IdRac = rs.getInt("IdRac");
+                String Status = rs.getString("Status");
+                int brstavki = rs.getInt("BrojStavki");
+                int DozvMinus = rs.getInt("DozvMinus");
+                int Stanje = rs.getInt("Stanje");
+                System.out.println(String.format("%d\t%s\t%d\t%d\t%d", IdRac, Status, brstavki, DozvMinus, Stanje));
+            }
+        }
+    }
+    public void zadatak(int idFil, int idRac) throws SQLException{
+        try ( Connection connection = DriverManager.getConnection(connectionString);  Statement s = connection.createStatement()) {
+       
+        String upit3="SELECT IdRac,Stanje FROM Racun WHERE IdKom=? AND Stanje>0";
+        PreparedStatement ps2=connection.prepareStatement(upit3);
+        ResultSet rs=ps2.executeQuery();
+        int sum = 0;
+        while(rs.next())
+        {
+        sum+=rs.getInt("Stanje");
+        
+        }
+        String upit5="Updtate Racun SET Stanje = 0,Status = U  WHERE IdFil=? AND Stanje>0";
+        PreparedStatement ps3=connection.prepareStatement(upit5);
+        ResultSet rs1=ps2.executeQuery();
+        
+        String upit4="UPDATE Racun SET Stanje=Stanje+? WHERE IdRac=?";
+        PreparedStatement st2=connection.prepareStatement(upit4);
+        st2.setInt(1,sum);
+        st2.executeUpdate();
+        connection.commit();
+        connection.setAutoCommit(true);
+    }catch(SQLException e) {System.out.println("greska");}
+        System.out.println("Uspesno");
+        
+    }
 
 }
--- src/main/java/rs/edu/matgim/zadatak/Program.java
+++ src/main/java/rs/edu/matgim/zadatak/Program.java
@@ -1,11 +1,14 @@
 package rs.edu.matgim.zadatak;
 
+import java.sql.SQLException;
+
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         DB _db = new DB();
         _db.printKomitent();
+        _db.printPositiveRacun();
         
     }
 }
